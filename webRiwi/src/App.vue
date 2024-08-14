7fb3105c0cf70519cc483d93780684542523560e
Harol <132421273+Harolin-23@users.noreply.github.com>
2024-08-14 15:58:51 -0500

    Add files via upload

---
+++ webRiwi/src/App.vue
@@ -0,0 +1,23 @@
+
+
+
+
+
+<script setup lang="ts">
+
+    let count = 0;
+
+</script>
+
+<template>
+    <div>
+      <h1>Hello world</h1>
+    </div>
+</template>
+
+
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
