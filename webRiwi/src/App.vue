39f8429152b72ab67f95371afa8ab89e48383118
Harol <hchaverra26@gmail.com>
2024-08-14 19:57:40 -0500

    "Updated checksum in vite.js build hook"

--- webRiwi/src/App.vue
+++ webRiwi/src/App.vue
@@ -1,4 +1,5 @@
 
+```vue
 <template>
     <div class="container-web">
         <div class="lef-sect">
@@ -7,24 +8,69 @@
         <div class="rigth-sect">
             <Header />
             <ProfileCard 
-                nombre="John Doe"
+                nombre="Harol Chaverra"
                 imagen="../src/assets/me.jpg"
             />
+        
+            <div class="actions-status">
+                <Buttoms
+                    size="medium"
+                    icon="fa-regular fa-user"
+                    text="Profile"
+                    :active="activeButton === 'profile'"
+                    @click="setActiveButton('profile')"
+                />
+                <Buttoms
+                    size="medium"
+                    icon="fa-solid fa-users"
+                    text="Team"
+                    :active="activeButton === 'team'"
+                    @click="setActiveButton('team')"
+                />
+                <Buttoms
+                    size="medium"
+                    icon="fa-solid fa-table-cells-large"
+                    text="Projects"
+                    :active="activeButton === 'projects'"
+                    @click="setActiveButton('projects')"
+                />
+                <Buttoms
+                    size="medium"
+                    icon="fa-solid fa-link"
+                    text="Connections"
+                    :active="activeButton === 'connections'"
+                    @click="setActiveButton('connections')"
+                />
+            </div>
         </div>
-      
     </div>
 </template>
 
 <script setup lang="ts">
-    import "./global/gen.scss";
-    import Menu from './components/nav-bar/Menu.vue'
-    import Header from './components/header/Header.vue'
-    import ProfileCard from './components/banner-Sesion/Account.vue'
-    let count = 0;
-    components: {
-    ProfileCard
-  }
-</script>
-<style >
+import "./global/gen.scss";
+import Menu from './components/nav-bar/Menu.vue';
+import Header from './components/header/Header.vue';
+import ProfileCard from './components/banner-Sesion/Account.vue';
+import Buttoms from './components/buttons/Buttoms.vue';
+import { ref } from "vue";
 
+const activeButton = ref<string | null>('profile');
+
+function setActiveButton(button: string) {
+    activeButton.value = button;
+}
+
+  
+
+</script>
+
+<style scoped>
+.actions-status {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    gap: 12px;
+    align-items: center;
+    padding-left: 12px;
+}
 </style>
