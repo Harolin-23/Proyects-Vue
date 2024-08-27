9e3e29fcb7ae1ca7b1a1eb5f3046e2219ba00258
Harol <hchaverra26@gmail.com>
2024-08-27 14:32:14 -0500

    chore: Add GitHub Actions workflow for deploying PelisVue content to Pages

--- PelisVue/vite.config.ts
+++ PelisVue/vite.config.ts
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+   base: 'https://Harolin-23.github.io/Proyects-Vue/PelisVue/'
 })
