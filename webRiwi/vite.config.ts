26ffab19af8f3a9398eb8f2750d3d7402a37ef68
Harol <hchaverra26@gmail.com>
2024-08-28 10:31:43 -0500

    chore: Remove base URL from vite.config.ts

--- webRiwi/vite.config.ts
+++ webRiwi/vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://Harolin-23.github.io/Proyects-Vue/',
   build: {
     outDir: 'dist',
   }
